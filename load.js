5776c7657eb3d4fc43d88404e53c398f08d67175
hartogsmith <ronald.d.smith@gmail.com>
2018-05-03 11:17:51 -0500

    Update load.js

--- load.js
+++ load.js
@@ -1,25 +1,31 @@
+/* Coded by Eren SOYLU @ 2016 
+This frontend aims getting data from Google Spreadsheets with .getJSON method and uses that data in jQuery DataTables
+*/
 $(document).ready(function() {
-      var gSheetArray = [];
-      //https://docs.google.com/spreadsheets/d/e/2PACX-1vQf7ogxoXQsr6EJgq7FYRtA9Knsp6PZCLuVunlW53sNdUTwh3XyV7kIKcJ_rcr8omofhWvRG-iBOREo/pubhtml
-      $.getJSON('https://spreadsheets.google.com/feeds/list/1GhrcWHv4ZRWZn55LyyG1cY0dkyo8Zj0mk8F8maWBXW0/od6/public/values?alt=json', function(json) {
-        for (var i = 0; i < json.feed.entry.length; i++) {
-          var iArray = [];
-          iArray.push(json.feed.entry[i].gsx$name.$t);
-          //iArray.push('<a href="'+json.feed.entry[i].gsx$url.$t+ '">header</a>');
-          gSheetArray.push(iArray);
-          $("#dataContent").append("<h2>" + json.feed.entry[i].gsx$headline.$t + "</h2>");
-          $("#dataContent").append("<p class="body">" + json.feed.entry[i].gsx$body.$t + "</p>");
-          $("#dataContent").append('<img src="' + json.feed.entry[i].gsx$header.$t + '" class="img2 img-responsive">');
-          
-        }
-        $('#example').DataTable( {
-        data: gSheetArray,
-        columns: [
-            { title: "headline" },
-            { title: "header" },
-            { title: "body" }
-        ]
-    } );
+  var tableArray = [];
+  /* First parameter in .getJSON method is published google spreadsheet's modified link for getting data formed with JSON. Tricky key is putting key between link template over here : https://spreadsheets.google.com/feeds/list/PUT-KEY_HERE/od6/public/values?alt=json  | You might be get key when you published your data spreadsheet. The key inside the URL. After getting JSON object you can investigate which key represent data. In this example json.feed.entry is an array that includes my data. Your solution should look similar like that.
+      */
+  $.getJSON(
+    "https://spreadsheets.google.com/feeds/list/1GhrcWHv4ZRWZn55LyyG1cY0dkyo8Zj0mk8F8maWBXW0/od6/public/values?alt=json",
+    function(json) {
+      for (var i = 0; i < json.feed.entry.length; i++) {
+        var iArray = [];
+        iArray.push(json.feed.entry[i].gsx$headline.$t);
+        //iArray.push('<a href="' + json.feed.entry[i].gsx$url.$t + '">URL</a>');
+        tableArray.push(iArray);
+        $("#dataContent").append(
+          "<h2>" + json.feed.entry[i].gsx$headline.$t + "</h2>"
+        );
+        $("#dataContent").append(
+          '<img src="' +
+            json.feed.entry[i].gsx$header.$t +
+            '" class="img2 img-responsive">'
+        );
+      }
+      $("#example").DataTable({
+        data: tableArray,
+        columns: [{ title: "headline" }, { title: "header" }, { title: "body" }]
       });
-    
+    }
+  );
 });
